4e1b94edb83341ffdbeeca31ff15b9c2a876f02a
Facundo <palavesinofacundo@gmail.com>
2024-11-28 16:34:44 -0300

    cors config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()], server: {
+    proxy: {
+        '/api': {
+            target: 'https://app-94pg.onrender.com', // URL de tu backend
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ''), 
+        },
+    },
+},
+});
